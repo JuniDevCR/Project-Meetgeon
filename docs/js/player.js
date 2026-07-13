ad36bfcf736421648b8e180a0d721ef1379fdb61
JuniDevCR <esteban.alvarado.suarez@gmail.com>
2026-07-13 12:29:22 -0600

    cambio en el nombre de la url de la imagen

--- docs/js/player.js
+++ docs/js/player.js
@@ -1,5 +1,5 @@
 const player = new Image();
-player.src = "/front-end/assets/qwerty.webp";
+player.src = "/docs/assets/qwerty.webp";
 
 player.onload = function () {
   context.drawImage(player, 0, 0, 100, 100);
